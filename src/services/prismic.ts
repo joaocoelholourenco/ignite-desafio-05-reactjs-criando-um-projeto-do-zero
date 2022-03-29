dbfae7c57f044f72def0ed7e6d62492bcc600ac3
João Vitor <joaovitorcoelho13@gmail.com>
2022-03-29 12:55:46 -0300

    chore: setup prismic and style global

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,10 +1,16 @@
-import Prismic from '@prismicio/client';
-import { DefaultClient } from '@prismicio/client/types/client';
+import * as prismic from '@prismicio/client';
+import { Client } from '@prismicio/client';
+import { enableAutoPreviews } from '@prismicio/next';
 
-export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
+export function getPrismicClient(req?: unknown): Client {
+  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  });
+
+  enableAutoPreviews({
+    client,
     req,
   });
 
-  return prismic;
+  return client;
 }
